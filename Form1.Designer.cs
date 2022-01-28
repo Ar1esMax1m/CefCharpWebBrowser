2e7d7ec73735e8ef9b29e1376894edb2e6de9b8b
KalininMN <ar1esmax1mwork@gmail.com>
2022-01-28 22:06:56 +0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace WebSharp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chromiumWebBrowser1 = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.urlField = new System.Windows.Forms.TextBox();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnReload = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chromiumWebBrowser1
+            // 
+            this.chromiumWebBrowser1.ActivateBrowserOnCreation = false;
+            this.chromiumWebBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+// TODO: Code generation for '' failed because of Exception 'Invalid Primitive Type: System.IntPtr. Consider using CodeObjectCreateExpression.'.
+            this.chromiumWebBrowser1.Location = new System.Drawing.Point(0, 74);
+            this.chromiumWebBrowser1.Name = "chromiumWebBrowser1";
+            this.chromiumWebBrowser1.Size = new System.Drawing.Size(959, 415);
+            this.chromiumWebBrowser1.TabIndex = 0;
+            this.chromiumWebBrowser1.LoadingStateChanged += new System.EventHandler<CefSharp.LoadingStateChangedEventArgs>(this.chromiumWebBrowser1_LoadingStateChanged);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSearch.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSearch.FlatAppearance.BorderSize = 0;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnSearch.Font = new System.Drawing.Font("Arial Narrow", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(150)));
+            this.btnSearch.Location = new System.Drawing.Point(807, 26);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(90, 30);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // urlField
+            // 
+            this.urlField.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.urlField.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.urlField.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.urlField.Location = new System.Drawing.Point(91, 28);
+            this.urlField.Name = "urlField";
+            this.urlField.Size = new System.Drawing.Size(710, 28);
+            this.urlField.TabIndex = 4;
+            this.urlField.Text = this.chromiumWebBrowser1.Address;
+            this.urlField.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.urlField_KeyPress);
+            // 
+            // btnForward
+            // 
+            this.btnForward.BackgroundImage = global::WebSharp2.Properties.Resources.arrow_forward;
+            this.btnForward.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnForward.FlatAppearance.BorderSize = 0;
+            this.btnForward.Location = new System.Drawing.Point(41, 27);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(30, 30);
+            this.btnForward.TabIndex = 5;
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.btnBack);
+            this.panel1.Controls.Add(this.btnReload);
+            this.panel1.Controls.Add(this.urlField);
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Controls.Add(this.btnForward);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.panel1.Size = new System.Drawing.Size(959, 74);
+            this.panel1.TabIndex = 6;
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnBack.BackgroundImage = global::WebSharp2.Properties.Resources.arrow_back;
+            this.btnBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnBack.Location = new System.Drawing.Point(9, 27);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(30, 30);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnReload
+            // 
+            this.btnReload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReload.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnReload.BackgroundImage = global::WebSharp2.Properties.Resources.reload1;
+            this.btnReload.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnReload.FlatAppearance.BorderSize = 0;
+            this.btnReload.Location = new System.Drawing.Point(900, 26);
+            this.btnReload.Margin = new System.Windows.Forms.Padding(0);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(30, 30);
+            this.btnReload.TabIndex = 2;
+            this.btnReload.UseVisualStyleBackColor = false;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(960, 507);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.chromiumWebBrowser1);
+            this.Name = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CefSharp.WinForms.ChromiumWebBrowser chromiumWebBrowser1;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnReload;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox urlField;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using System.Runtime.InteropServices;
+
+namespace WebSharp2
+{
+    public partial class Form1 : Form
+    {
+        public System.Windows.Forms.Keys KeyCode { get; }
+        
+        public Form1()
+        {
+            Cef.EnableHighDPISupport();
+            InitializeComponent();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        { 
+            if (chromiumWebBrowser1.CanGoBack)
+            {
+                chromiumWebBrowser1.Back();
+            }
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            if (chromiumWebBrowser1.CanGoForward)
+            {
+                chromiumWebBrowser1.Forward();
+            }
+        }
+        private void urlField_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter) btnSearch_Click(sender, e);
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            chromiumWebBrowser1.Load(urlField.Text);
+        }
+
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            chromiumWebBrowser1.Refresh();
+        }
+
+        private void chromiumWebBrowser1_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+            if(this.chromiumWebBrowser1.Address != "") urlField.Text = this.chromiumWebBrowser1.Address;
+        }
+    }
+}
